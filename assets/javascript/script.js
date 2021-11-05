1395ebc7c435a98753f8af287e253ae3f984e50f
NickNocc <89278623+NickNocc@users.noreply.github.com>
2021-11-05 10:58:29 -0700

    Fixed repo

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// Array filled with: Question, Answer, possible other answers
+
+// Need a taskhandler that looks at what is clicked and gives you feedback based on what you click (right or wrong answers)
+
+// Section that changes the h1 element into the question
+// Also needs to inject a div for the possible answers to go into can use this later for highscore entry
+
+// Section that saves highscores to local memory
+
+// Timer that counts down from 1 minute for the timed portion of the quiz
+// Also needs to be called later to set highscores
+fdsfdsf
